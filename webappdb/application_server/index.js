16b477673a5a2cb9ed53b634fe220eeff1896cbc
Darren Seet <darren_seet@oredi.com>
2018-03-11 08:43:03 -0400

    Initial Commit

---
+++ webappdb/application_server/index.js
@@ -0,0 +1,63 @@
+'use strict'
+
+// Load our dependencies
+const bodyParser = require('body-parser');
+const express = require('express');
+const mongo = require('mongodb');
+
+// Setup database and server constants
+const DB_NAME = 'word_database';
+const DB_HOST = process.env.DB_HOST || 'localhost:27017';
+const COLLECTION_NAME = 'words';
+const SERVER_PORT = 8000;
+
+// Create our app, database clients, and the word list array
+const app = express();
+const connection = mongo.MongoClient;
+const dbUri = `mongodb://${DB_HOST}`;
+const words = [];
+
+// Setup our templating engine and for data parser
+app.set('view engine', 'pug');
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Load all words that are in the database
+function loadWordsFromDatabase() {
+    return connection.connect(dbUri)
+        .then((client) => {
+            return client.db(DB_NAME).collection(COLLECTION_NAME).find({}).toArray();
+        })
+        .then((docs) => {
+            words.push.apply(words, docs.map(doc => doc.word));
+            return words;
+        });
+}
+
+// Our main landing page handler
+app.get('/', (req, res) => {
+    res.render('index', { words: words });
+});
+
+// Handler for POSTing a new word
+app.post('/new', (req, res) => {
+    const word = req.body.word;
+
+    console.info(`Got word: ${word}`);
+    if (word) {
+        connection.connect(dbUri).then((client) => {
+            client.db(DB_NAME).collection(COLLECTION_NAME).insertOne({ word }, () => {
+                client.close();
+                words.push(word);
+                res.redirect('/');
+            });
+        });
+    }
+});
+
+// Start everything by loading words and then starting the server
+loadWordsFromDatabase().then((words) => {
+    console.info(`Data loaded from database (${words.length} word${words.length > 0 ? 's' : ''})`);
+    app.listen(SERVER_PORT, () => {
+        console.info("Server started on port %d...", SERVER_PORT);
+    });
+});
