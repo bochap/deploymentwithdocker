adedf414f32e0a170b9d43b103279d9f4aebff39
Darren Seet <darren_seet@oredi.com>
2018-03-12 08:27:15 -0400

    Fix typos

--- webappdb_clustered/web_server/start_nginx.sh
+++ webappdb_clustered/web_server/start_nginx.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-export DNS_RESOLVERS=$(cat /etc/resolv.conf | grep 'nameserver'| awk '{ print $2 }' | xargs echo)
+export DNS_RESOLVERS=$(cat /etc/resolv.conf | grep 'nameserver' | awk '{ print $2 }' | xargs echo)
 
 cat /etc/nginx/conf.d/nginx_main_site.conf.template | envsubst '$DNS_RESOLVERS $APP_NAME' >> /etc/nginx/conf.d/nginx_main_site.conf
 
