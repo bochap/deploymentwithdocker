74c07525474900f97ac19771ed5da29700572eca
Darren Seet <darren_seet@oredi.com>
2018-03-16 08:56:59 -0400

    Fix typo for port

--- webappdb_clustered/web_server/start_nginx.sh
+++ webappdb_clustered/web_server/start_nginx.sh
@@ -2,9 +2,6 @@
 
 export DNS_RESOLVERS=$(cat /etc/resolv.conf | grep 'nameserver' | awk '{ print $2 }' | xargs echo)
 
-echo $DNS_RESOLVERS
-echo $APP_NAME
-
 cat /etc/nginx/conf.d/nginx_main_site.conf.template | envsubst '$DNS_RESOLVERS $APP_NAME' >> /etc/nginx/conf.d/nginx_main_site.conf
 
 nginx -g 'daemon off;'
